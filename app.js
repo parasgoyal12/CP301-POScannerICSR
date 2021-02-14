693f3536e81e492979c55034fdd34cdd7bb6b71c
Paras Goyal <parasgoyal12@gmail.com>
2021-02-14 19:57:31 +0530

    Initialize server on PORT 3000

--- app.js
+++ app.js
@@ -38,4 +38,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+PORT= process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+  console.log("Listening on PORT 3000");
+});
 module.exports = app;
