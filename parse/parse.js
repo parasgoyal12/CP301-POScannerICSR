fd160b1f344855594f3c43ad4604bb6cb220d2f8
Akshay Gahlot <akshaygahlot73@gmail.com>
2021-02-16 13:49:31 +0530

    Modified parser

--- parse/parse.js
+++ parse/parse.js
@@ -2,29 +2,36 @@ const fs = require('fs');
 
 fs.readFile('res1.json', (err, data) => {
     if (err) throw err;
+
+    // res1
     let str = JSON.parse(data);
     str = str.text;
 
+    // res2
+    // let str = JSON.parse(data).fullTextAnnotation.text;
+
+
     // Assumptions for rgx in comments, won't work without these
 
-    // First instance is always date (as in samples)
+    // xx.xx.xxxx format, First instance is always date (as in samples)
     const dateRgx = /\d{2}[.]\d{2}[.]\d{4}/;
     const date = (str.match(dateRgx))[0];
 
     // Total\nAmt
-    const amountRgx = /Total[" "-:;]*\n[\d,.]*/ 
+    const amountRgx = /Total[" "-:;]*[\n]*[\d,.]*/; 
     let amount = (str.match(amountRgx))[0];
-    amount = amount.substring(amount.match(/Total[" "-:;]*\n/)[0].length, amount.length);
+    amount = amount.substring(amount.match(/[\d]/).index, amount.length);
 
     // PO number starts with digit and ??
-    const PONoRgx = /PO No[:\n][" "]*[\d-,/\w]*/;
+    const PONoRgx = /PO No[.:\n]*[" "]*[\d-,/\w]*/;
     let PO = (str.match(PONoRgx))[0];
     PO = PO.substring(PO.search(/[\d]/), PO.length);
 
     // Supple of 'item'
-    const itemRgx = /Supply[" "]*of[" "-.:,]*'[\w" "\d-.,:]*'/;
+    const itemRgx = /Supply[" "]*of[" "-.:,']*[\w" "\d-.,:]*'/;
     let item = (str.match(itemRgx))[0];
-    item = item.substring(item.search(/'/) + 1, item.length - 1);
+    item = item.substring(item.match(/Supply[" "]*of[" "-.:,']*/)[0].length, item.length - 1);
+    // item = item.substring(item.search(/'/) + 1, item.length - 1);
 
     // FOR : Deaprtment of .., (no comma in dept name & comma after dept name)
     const deptRgx = /FOR[" ":]*Department of[" "]*[\w" "\d]*/;
@@ -42,9 +49,3 @@ fs.readFile('res1.json', (err, data) => {
 
     console.log(date, '\n', amount, '\n', PO, '\n', item, '\n', dept, '\n', supplier, '\n', indentator);
 });
-
-
-
-
-
-
