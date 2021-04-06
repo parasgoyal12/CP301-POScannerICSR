0883a5d8e2e7e8e02c79711a417a4858ef440c3c
Adarsh Kumar <2018CSB1066@iitrpr.ac.in>
2021-04-06 17:51:39 +0530

    view users page added

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,26 @@
+const {User}  = require('../models/users');
 
 exports.getIndex = (req,res,next) =>{
     res.render('admin/index',{title:'Admin',user:req.user,successFlash:req.flash("success")});
+};
+
+exports.getRegUsers = (req,res,next) =>{
+    User.find()
+        .then((result) =>{
+            res.render('admin/registeredUsers',{title:'Registered Users',users:result,user:req.user});
+        })
+};
+
+exports.deleteUser = (req,res,next)=>{
+    User.findByIdAndDelete(req.params.id)
+    .then(resp=>{
+        console.log(resp);
+        req.flash("success",`${resp.name} Deleted Succesfully!`);
+        res.redirect('/admin/registeredUsers');
+    })
+    .catch((err)=>{
+        console.log(err);
+        req.flash("success","Delete Failed");
+        res.redirect("/admin/registeredUsers");
+    })
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,7 @@ const admin = require('../controllers/admin');
 const {isAdmin,isLoggedIn} = require('../middleware/hasAuth');
 
 router.get('/',isLoggedIn,isAdmin,admin.getIndex);
+router.get('/registeredUsers',isLoggedIn,isAdmin,admin.getRegUsers);
+router.get('/deleteUser/:id',isLoggedIn,isAdmin,admin.deleteUser);
 
 module.exports = router;
