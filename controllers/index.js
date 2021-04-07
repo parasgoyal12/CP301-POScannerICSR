b89effd5b9fbdb02280c814ef60fbbe88f412bc0
Paras Goyal <parasgoyal12@gmail.com>
2021-04-07 18:41:13 +0530

    Correct Typo

--- controllers/index.js
+++ controllers/index.js
@@ -90,7 +90,7 @@ exports.submitConfirmationPage= async (req,res,next)=>{
     });
     let formResponse = req.body;
     // console.log(req.body);
-    formResponse.fileName = `${keys.clientURL}/uploads/${formResponse.fileName}`;
+    formResponse.fileName = `${keys.clientUrl}/uploads/${formResponse.fileName}`;
     formResponse.user = req.user.name;
     delete formResponse.submit;
     try{
